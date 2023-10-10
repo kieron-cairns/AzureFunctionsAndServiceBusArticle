a9863c288e4385505f3b9b28667a371c11355e42
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-10-10 19:08:13 +0100

    CHANGE: Depdency injection and config settings implemented

---
+++ FNQueueFormSubmission/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(FNQueueFormSubmission.Startup))]
+
+
+namespace FNQueueFormSubmission
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+
+            builder.Services.AddSingleton<IAzureKeyVaultWrapper>(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                string keyVaultUrl = config["AzureKeyVaultConfig:KVUrl"];
+                string clientSecretId = config["AzureKeyVaultConfig:KVSecretClientValue"];
+
+                var clientSecretCredential = new DefaultAzureCredential();
+                var secretClient = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
+                return new AzureKeyVaultWrapper(secretClient);
+            });
+
+            builder.Services.AddSingleton<IAzureSecretClientWrapper, AzureSecretClientWrapper>();
+        }
+    }
+}
