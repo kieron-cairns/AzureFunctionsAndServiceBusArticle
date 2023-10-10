f68840a6e4da27d0a4ac9217de39dd89ea0d5ffd
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-10-10 18:37:21 +0100

    Add project files.

---
+++ FNQueueFormSubmission/FNQueueFormSubmission.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FNQueueFormSubmission
+{
+    public class FNQueueFormSubmission
+    {
+        [FunctionName("QueueFormSubmission")]
+        public async Task<IActionResult> Run(
+         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+         [ServiceBus("form-submissions", Connection = "ServiceBusConnectionString")] IAsyncCollector<string> collector,
+         ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            await collector.AddAsync(requestBody); // Queue the data to Service Bus
+            return new OkObjectResult("Form submission received and is being processed.");
+        }
+
+    }
+}
