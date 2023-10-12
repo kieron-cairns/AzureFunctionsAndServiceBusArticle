87940f82dbaa5615563e278f5d31b0d9b03bab16
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-10-12 16:25:32 +0100

    CHANGE: Mocked StreamReaderWrapper tests re-implemented

--- FNReCaptchaVerificationTests/Wrappers/StreamReaderWrapperTests.cs
+++ FNReCaptchaVerificationTests/Wrappers/StreamReaderWrapperTests.cs
@@ -1,7 +1,9 @@
 ﻿using FNReCaptchaVerification.Utilities;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +12,23 @@ namespace FNReCaptchaVerificationTests.Wrappers
     public class StreamReaderWrapperTests
     {
         [Fact]
-        public async Task ReadToEndAsync_ReturnsCorrectString()
+        public async Task ReadToEndAsync_ReturnsExpectedContent()
         {
-            var testString = "Test Content";
-            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(testString));
-            var streamReaderWrapper = new StreamReaderWrapper(memoryStream);
+            // Arrange
+            var sampleContent = "Sample Stream Content";
+            var mockStreamReader = new Mock<StreamReader>(Stream.Null); // Stream.Null is just a placeholder since we'll mock the method call
 
-            var result = await streamReaderWrapper.ReadToEndAsync();
+            mockStreamReader.Setup(sr => sr.ReadToEndAsync()).ReturnsAsync(sampleContent);
 
-            Assert.Equal(testString, result);
+            // Here we use a little trick: We'll substitute the private _streamReader field of StreamReaderWrapper with our mock
+            var wrapper = new StreamReaderWrapper(Stream.Null); // Stream.Null is a placeholder, since the mock will intercept the call
+            typeof(StreamReaderWrapper).GetField("_streamReader", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(wrapper, mockStreamReader.Object);
+
+            // Act
+            var content = await wrapper.ReadToEndAsync();
+
+            // Assert
+            Assert.Equal(sampleContent, content);
         }
     }
 }
