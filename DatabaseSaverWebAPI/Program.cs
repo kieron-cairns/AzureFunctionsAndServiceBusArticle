55424ab7731f5184de2783c7bbac67f7d7b6a918
kieron-cairns <kieroncairns@protonmail.com>
2023-10-15 10:28:57 +0100

    CHANGE: Azure Key Vault config added to Program.cs for DI purposes

--- DatabaseSaverWebAPI/Program.cs
+++ DatabaseSaverWebAPI/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var keyVaultUrl = builder.Configuration["KVUrl"];
+
+if(!string.IsNullOrEmpty(keyVaultUrl))
+{
+    var credential = new DefaultAzureCredential();
+    builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUrl), credential)
+}
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
