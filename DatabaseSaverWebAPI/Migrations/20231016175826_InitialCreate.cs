0d6b34f0468a777d9cbf4398bf963b1928594508
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-10-16 19:17:57 +0100

    CHANGE: EF Core successfully integrated

---
+++ DatabaseSaverWebAPI/Migrations/20231016175826_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseSaverWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormSubmissions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Submitted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsContactFormSubmit = table.Column<bool>(type: "bit", nullable: false),
+                    IsNewsLetterSubmit = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormSubmissions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormSubmissions");
+        }
+    }
+}
--- DatabaseSaverWebAPI/Program.cs
+++ DatabaseSaverWebAPI/Program.cs
@@ -1,21 +1,18 @@
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using DatabaseSaverWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// var keyVaultUrl = builder.Configuration["KVURL"];
-
-var keyVaultUrl = Environment.GetEnvironmentVariable("KVURL");
-
 var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
 
-if(!string.IsNullOrEmpty(keyVaultUrl))
-{
+// Get the Key Vault URL
+var keyVaultUrl = Environment.GetEnvironmentVariable("KVURL");
 
-    logger.LogInformation("$KeyVaultUrl URL retrieved!");
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    logger.LogInformation("KeyVault URL retrieved!");
 
     var credential = new DefaultAzureCredential();
     builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUrl), credential);
@@ -25,21 +22,26 @@ else
     logger.LogInformation("KeyVault URL not found in config");
 }
 
-var sqlConnectionString = Environment.GetEnvironmentVariable("KVSecretName");
+// Get SQL Connection String from Key Vault
+string secretName = Environment.GetEnvironmentVariable("KVSecretName") ?? "YourSecretName"; // Replace "YourSecretName" with the name of your secret if it's not in an environment variable
+
+var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+string sqlConnectionString = secretClient.GetSecret(secretName).Value.Value;
+
 if (!string.IsNullOrEmpty(sqlConnectionString))
 {
-
-    logger.LogInformation("SQL connection string retrieved");
+    logger.LogInformation("SQL connection string retrieved from Key Vault.");
 }
-else 
+else
 {
-    logger.LogInformation("SQL connection string retrieval error");
+    logger.LogInformation("SQL connection string retrieval error from Key Vault.");
 }
 
 builder.Services.AddDbContext<FormSubmissionContext>(options => options.UseSqlServer(sqlConnectionString));
 
-    builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
+// Swagger Configuration
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
