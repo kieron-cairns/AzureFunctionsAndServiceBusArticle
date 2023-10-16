efdc7e516943664533b7e1477c05d3c4817f1982
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-10-16 12:29:46 +0100

    CHANGE: Form submission class model added

---
+++ DatabaseSaverWebAPI/Models/FormSubmission.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseSaverWebAPI.Models
+{
+    public class FormSubmission
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Message { get; set; }
+
+        [Required]
+        public DateTime Submitted { get; set; }
+
+        public bool IsContactFormSubmit { get; set; }
+
+        public bool IsNewsLetterSubmit { get; set; }
+    }
+}
